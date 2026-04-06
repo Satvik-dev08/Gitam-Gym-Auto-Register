0d5d35370b27fcf5fb22b1700c1f746af1e0d508
Satvik-dev08 <satvikkolupoti@gmail.com>
2026-04-06 22:44:38 +0530

    ...ye im cooked

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const puppeteer = require('puppeteer');
 
     const browser = await puppeteer.launch({
       headless: "new",
-      executablePath: '/usr/bin/chromium-browser',
       args: ['--no-sandbox', '--disable-setuid-sandbox']
     });
 
